3d3262cfc94d517f89aa86b736d86b6680a56c91
Basma-Hassan95 <basmakhan220@gmail.com>
2025-08-21 22:45:07 +0500

    first-commit

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ function openWhatsApp(e) {
   }).then((result) => {
     if (result.isConfirmed) {
       window.open(
-        "https://api.whatsapp.com/send?phone=923180254231&text=Hello%20I%20want%20to%20know%20more%20about%20your%20products",
+        "https://api.whatsapp.com/send?phone=923404766631&text=Hello%20I%20want%20to%20know%20more%20about%20your%20products",
         "_blank"
       );
     }
